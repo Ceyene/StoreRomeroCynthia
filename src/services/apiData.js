2f9443878a072348166c8fb6548573957007f6b9
Cyn Romero <cyn.jaz.romero@gmail.com>
2020-12-29 15:11:29 -0300

    New Token

--- src/services/apiData.js
+++ src/services/apiData.js
@@ -1,7 +1,7 @@
 export const myHeaders = {
   "Content-Type": "application/json",
   Authorization:
-    "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZmQ5YWJhYjI2ZjdkMjAwMjA0MTE0Y2QiLCJpYXQiOjE2MDgxMDA3Nzl9.bjQRXSEcQuF_zFF_HS0NuNT0YHRE-UDAxlGe5q9EZdU",
+    "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZmViNmM4Y2Y3NjZmZTAwMTkxOWUwYzQiLCJpYXQiOjE2MDkyNjQyNjh9.GQiDxXmuMqfqcmmfElVVDCAgOj69hweTWELotAJL2zM",
 };
 export const baseUrl = "https://coding-challenge-api.aerolab.co/";
 
