bb34f31fbf27ec936c445147f6ce29c0dfc7b5bf
Cyn Romero <56238760+Ceyene@users.noreply.github.com>
2020-12-29 15:08:32 -0300

    Correction15: Secret Token

--- src/services/apiData.js
+++ src/services/apiData.js
@@ -1,7 +1,6 @@
 export const myHeaders = {
   "Content-Type": "application/json",
-  Authorization:
-    "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZmQ5YWJhYjI2ZjdkMjAwMjA0MTE0Y2QiLCJpYXQiOjE2MDgxMDA3Nzl9.bjQRXSEcQuF_zFF_HS0NuNT0YHRE-UDAxlGe5q9EZdU",
+  Authorization: MY_TOKEN,
 };
 export const baseUrl = "https://coding-challenge-api.aerolab.co/";
 
